25c2cd1b908745cd1342a5ec48c0ada9ba0cc991
Michael Kroon <kroonology@gmail.com>
2021-12-14 15:12:11 -0700

    chapter 12; play with paintComponent(); jpg and paint

--- src/exercises/MyDrawPanel.java
+++ src/exercises/MyDrawPanel.java
@@ -5,7 +5,19 @@ import javax.swing.*;
 
 class MyDrawPanel extends JPanel {
     public void paintComponent(Graphics g){
-        g.setColor(Color.orange);
-        g.fillRect(20, 50, 100, 100);
+//        g.setColor(Color.orange);
+//        g.fillRect(20, 50, 100, 100);
+
+//        Image image = new ImageIcon("exercises/catzilla.jpg").getImage();
+//        g.drawImage(image, 3, 4, this);
+
+        g.fillRect(0,0,this.getWidth(), this.getHeight());
+        int red = (int) (Math.random() * 256);
+        int green = (int) (Math.random() * 256);
+        int blue = (int) (Math.random() * 256);
+
+        Color randomColor = new Color(red, green, blue);
+        g.setColor(randomColor);
+        g.fillOval(70, 70, 100, 100);
     }
 }
