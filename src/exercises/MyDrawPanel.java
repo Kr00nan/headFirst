bdff7a28ebd1693ec6523df424b62ea703f193c3
Michael Kroon <kroonology@gmail.com>
2021-12-14 15:54:13 -0700

    chapter 12; cast graphic obj to graphic2d obj, randomize gradient coloring

--- src/exercises/MyDrawPanel.java
+++ src/exercises/MyDrawPanel.java
@@ -25,9 +25,27 @@ class MyDrawPanel extends JPanel {
 //        g.setColor(randomColor);
 //        g.fillOval(70, 70, 100, 100);
 
+        // creates gradient coloring and applies it to a circle
+//        Graphics2D g2d = (Graphics2D) g;
+//        GradientPaint gradient = new GradientPaint(70, 70, Color.blue, 150, 150, Color.orange);
+//        g2d.setPaint(gradient);
+//        g2d.fillOval(70, 70, 100, 100);
+
+        // same as above but randomizes the colors
         Graphics2D g2d = (Graphics2D) g;
-        GradientPaint gradient = new GradientPaint(70, 70, Color.blue, 150, 150, Color.orange);
+        int red = (int) (Math.random() * 256);
+        int green = (int) (Math.random() * 256);
+        int blue = (int) (Math.random() * 256);
+        Color startColor = new Color(red, green, blue);
+
+        red = (int) (Math.random() * 256);
+        green = (int) (Math.random() * 256);
+        blue = (int) (Math.random() * 256);
+        Color endColor = new Color(red, green, blue);
+
+        GradientPaint gradient = new GradientPaint(70, 70, startColor, 150, 150, endColor);
         g2d.setPaint(gradient);
         g2d.fillOval(70, 70, 100, 100);
+
     }
 }
