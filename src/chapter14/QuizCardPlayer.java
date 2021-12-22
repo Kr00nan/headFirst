7a3cef5d549fcc4394b46ffb514ba6349a95bd0f
Michael Kroon <kroonology@gmail.com>
2021-12-22 15:24:40 -0700

    chapter 14; QuizCardPlayer build showNextCard, makeCard, and OpenMenuListener

--- src/chapter14/QuizCardPlayer.java
+++ src/chapter14/QuizCardPlayer.java
@@ -1,9 +1,22 @@
 package chapter14;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
 
 public class QuizCardPlayer {
+
+    private JTextArea display;
+    private JTextArea answer;
+    private ArrayList<QuizCard> cardList;
+    private QuizCard currentCard;
+    private int currentCardIndex;
+    private JFrame frame;
+    private JButton nextButton;
+    private boolean isShowAnswer;
+
     public void go() {
         // build and display gui
     }
@@ -22,7 +35,10 @@ public class QuizCardPlayer {
         @Override
         public void actionPerformed(ActionEvent e) {
             // bring up a file dialog box
+            JFileChooser fileOpen = new JFileChooser();
+            fileOpen.showOpenDialog(frame);
             // let the user navigate to and choose a card set to open
+            loadFile(fileOpen.getSelectedFile());
         }
     }
 
@@ -35,9 +51,19 @@ public class QuizCardPlayer {
 
     private void makeCard(String lineToParse) {
         // called by the loadFile method, takes a line from the text file
+        String[] result = lineToParse.split(" / ");
         // and parses into two pieces--question and answer--and creates a new QuizCard
+        QuizCard card = new QuizCard(result[0], result[1]);
         // and adds it to the ArrayList called CardList
+        cardList.add(card);
+        System.out.println("made a card");
     }
 
-
+    private void showNextCard() {
+        currentCard = cardList.get(currentCardIndex);
+        currentCardIndex++;
+        display.setText(currentCard.getQuestion());
+        nextButton.setText("Show Answer");
+        isShowAnswer = true;
+    }
 }
