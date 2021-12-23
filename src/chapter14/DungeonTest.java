8796c0280cf5e1e664920cf47a24145d23f6e175
Michael Kroon <kroonology@gmail.com>
2021-12-23 13:41:18 -0700

    chapter 14; CodeMagnets DungeonGame

---
+++ src/chapter14/DungeonTest.java
@@ -0,0 +1,27 @@
+package chapter14;
+
+import java.io.*;
+
+public class DungeonTest {
+    public static void main(String[] args) {
+        DungeonGame d = new DungeonGame();
+        System.out.println(d.getX() + d.getY() + d.getZ());
+        try {
+
+            FileOutputStream fos = new FileOutputStream("dg.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(d);
+            oos.close();
+
+            FileInputStream fis = new FileInputStream("dg.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            d = (DungeonGame) ois.readObject();
+
+
+            System.out.println(d.getX() + d.getY() + d.getZ());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
