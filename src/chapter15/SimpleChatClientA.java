0201b1967236f6a0c054f00f30bb80afc1fb6627
Michael Kroon <kroonology@gmail.com>
2021-12-27 14:35:40 -0700

    chapter 15; simple chat client exercise, final code

--- src/chapter15/SimpleChatClientA.java
+++ src/chapter15/SimpleChatClientA.java
@@ -1,8 +1,10 @@
 package chapter15;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -14,12 +16,31 @@ public class SimpleChatClientA {
 
     public void go() {
         // make gui and register a listener with the send button
+        JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         // call the setUpNetworking() method
+        setUpNetworking();
+        frame.setSize(400, 500);
+        frame.setVisible(true);
     }
 
     private void setUpNetworking() {
         // make a Socket, then make a PrintWriter
-        // assign the PrintWriter to writer instance variable
+        try {
+            Socket socket = new Socket("127.0.0.1", 5000);
+            // assign the PrintWriter to writer instance variable
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("networking established");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
     }
 
     public class SendButtonListener implements ActionListener {
@@ -27,6 +48,19 @@ public class SimpleChatClientA {
         public void actionPerformed(ActionEvent e) {
             // get the text from the text field and
             // send it to the server using the writer (a PrinterWriter)
+            try {
+                writer.println(outgoing.getText());
+                writer.flush();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            outgoing.setText("");
+            outgoing.requestFocus();
         }
+    }
+
+    public static void main(String[] args) {
+        new SimpleChatClientA().go();
     }
 }
