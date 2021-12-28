17f63a4c27d2f8559fa4ba3d5d6253e534ffbfcd
Michael Kroon <kroonology@gmail.com>
2021-12-28 14:30:12 -0700

    chapter 16; Jukebox code, implement Collections.sort(), create Song class

--- src/chapter16/Jukebox1.java
+++ src/chapter16/Jukebox1.java
@@ -14,6 +14,8 @@ public class Jukebox1 {
     public void go() {
         getSongs();
         System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println(songList);
     }
 
     public void getSongs() {
